c19ba693b36dbf8d3f6883d7ed52a474f15d2a74
catageek <catageek@free.fr>
2013-09-28 08:03:28 +0200

    Cancel deprecation of some methods

--- src/com/github/catageek/ByteCartAPI/Event/UpdaterSetStationEvent.java
+++ src/com/github/catageek/ByteCartAPI/Event/UpdaterSetStationEvent.java
@@ -37,15 +37,7 @@ public class UpdaterSetStationEvent extends UpdaterClearStationEvent {
 	/**
 	 * @return the newAddress
 	 */
-	@Deprecated
 	public Address getNewAddress() {
 		return newAddress;
-	}
-
-	/**
-	 * @return the newAddress
-	 */
-	public String getAddress() {
-		return newAddress.toString();
 	}
 }
