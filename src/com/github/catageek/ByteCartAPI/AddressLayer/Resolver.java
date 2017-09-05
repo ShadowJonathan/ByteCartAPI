2317e72fb67e9550f8ad592cfca5dd27db957772
Pokechu22 <Pokechu022@gmail.com>
2017-09-05 12:32:42 -0700

    Add Resolver.getNames and getMatchingNames

--- src/com/github/catageek/ByteCartAPI/AddressLayer/Resolver.java
+++ src/com/github/catageek/ByteCartAPI/AddressLayer/Resolver.java
@@ -1,14 +1,31 @@
 package com.github.catageek.ByteCartAPI.AddressLayer;
 
+import java.util.List;
+
 /**
  * Represent a class that can get an address from a name
  */
 public interface Resolver {
 	/**
 	 * Resolve the name
-	 * 
+	 *
 	 * @param name the name to resolve
 	 * @return the address corresponding to the name, or null
 	 */
 	String resolve(String name);
+
+	/**
+	 * Gets a list of all names
+	 *
+	 * @return all available names
+	 */
+	List<String> getNames();
+
+	/**
+	 * Gets a list of names starting with the given text
+	 *
+	 * @param prefix the starting text
+	 * @return All matching names
+	 */
+	List<String> getMatchingNames(String prefix);
 }
